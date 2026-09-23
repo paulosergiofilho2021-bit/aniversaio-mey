2b07b3d05ae21f32bdb1b4b586943de79fa5cfb9
Paulo <paulosergiofilho2021@gmail.com>
2026-09-23 09:40:43 -0300

    Versão final

--- pagina-4/script.js
+++ pagina-4/script.js
@@ -48,10 +48,41 @@ observer.observe(boxtMessage1);
 observer.observe(images);
 observer.observe(messageKae);
 
-
 const gift = document.querySelector(".gift");
 const imgRosa = document.querySelector(".image-rosa");
 gift.addEventListener("click", () => {
   gift.style.opacity = "0";
   imgRosa.style.opacity = "1";
 });
+
+let counter = 0;
+let counterOpacity = 5;
+const phraseHeart = document.querySelector(".phrase-heart");
+const heart = document.querySelector(".heart");
+const button1 = document.querySelector(".buttton-1");
+const button2 = document.querySelector(".buttton-2");
+const subBar = document.querySelector(".sub-bar");
+const opacityCounter = document.querySelector(".opacity-counter");
+
+button1.addEventListener("click", () => {
+  if (counter < 100) {
+    counterOpacity -= 1;
+    counter += 20;
+    subBar.style.height = `${counter}%`;
+    opacityCounter.textContent = `${counter}%`;
+    phraseHeart.style.filter = `blur(${counterOpacity}px)`;
+    heart.style.filter = `blur(${counterOpacity}px)`;
+  }
+});
+
+button2.addEventListener("click", () => {
+  if (counter > 0) {
+    counterOpacity += 1;
+    counter -= 20;
+    subBar.style.height = `${counter}%`;
+    opacityCounter.textContent = `${counter}%`;
+    phraseHeart.style.filter = `blur(${counterOpacity}px)`;
+    heart.style.filter = `blur(${counterOpacity}px)`;
+  }
+});
+console.log(counter)
